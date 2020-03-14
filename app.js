8ebf167664cd949ffa9b15054abc26d171b43073
Paweł Zarzycki <zaarzyk@gmail.com>
2020-03-14 15:12:12 +0000

    Dodano połączenie z usługą Azure Storage

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ const logger = require('koa-logger')
 const index = require('./routes/index')
 const users = require('./routes/users')
 
+require('./store').init()
+
 // error handler
 onerror(app)
 
---
+++ store.js
@@ -0,0 +1,15 @@
+const storage = require('azure-storage')
+const service = storage.createTableService()
+const table = 'tasks'
+
+const init = async () => (
+  new Promise((resolve, reject) => {
+    service.createTableIfNotExists(table, (error, result, response) => {
+      !error ? resolve() : reject()
+    })
+  })
+)
+
+module.exports = {
+  init
+}
