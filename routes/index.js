32c7b50785f534664d7217d4312c87d7df9fb788
Paweł Zarzycki <zaarzyk@gmail.com>
2020-03-28 09:14:45 +0000

    Dodano wyświetlanie listy zadań

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,13 @@
+const store = require('../store')
 const router = require('koa-router')()
 
 router.get('/', async (ctx, next) => {
-  await ctx.render('index', {
-    title: 'Hello Koa 2!'
-  })
+  tasks = await store.listTasks()
+  await ctx.render('index', { tasks })
 })
+
+
+
 
 router.get('/string', async (ctx, next) => {
   ctx.body = 'koa2 string'
--- store.js
+++ store.js
@@ -2,6 +2,38 @@ const storage = require('azure-storage')
 const service = storage.createTableService()
 const table = 'tasks'
 
+const listTasks = async () => (
+  new Promise((resolve, reject) => {
+    const query = new storage.TableQuery()
+      .select(['title'])
+      .where('PartitionKey eq ?', 'task')
+
+    service.queryEntities(table, query, null, (error, result, response) => {
+      !error ? resolve(result.entries.map((entry) => ({
+        title: entry.title._
+      }))) : reject()
+    })
+  })
+)
+
+
+
+
+module.exports = {
+  init,
+  createTask,
+  listTasks
+}
+
+
+
+
+
+
+
+
+
+
 const init = async () => (
   new Promise((resolve, reject) => {
     service.createTableIfNotExists(table, (error, result, response) => {
